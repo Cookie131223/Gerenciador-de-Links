25b7c8d1fdeca92bbb418df9c54871bd00514541
Cookie131223 <pedropossid3@gmail.com>
2026-03-04 10:15:04 -0300

    primeira versao

---
+++ links/src/app/index/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "@/styles/colors";
+
+export const styles = StyleSheet.create({
+container:{
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+},
+
+  title :{
+color: colors.gray[900],
+fontSize: 22,
+
+
+  },
+})
