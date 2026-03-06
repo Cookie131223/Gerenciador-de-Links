f3c7dae2da6c6a7d0926b1f6354cd422f8eaf353
Cookie131223 <pedropossid3@gmail.com>
2026-03-06 09:58:15 -0300

    segunda versão finalizada

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,10 +1,11 @@
-import { View, Image, TouchableOpacity, FlatList } from 'react-native';
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from 'react-native';
 import { MaterialIcons} from '@expo/vector-icons';
 
 import { styles } from './styles';
 import { colors } from '@/styles/colors';
 
 import { Link } from '@/components/link';
+import { Option } from '@/components/option';
 import { Categories } from '@/components/categories';
 
 
@@ -23,11 +24,37 @@ export default function Index() {
       <Categories />
       
     <FlatList 
-    data={["1","2","3"]}
+    data={["1","2","3","4","5"]}
     keyExtractor={item => item}
     renderItem={() => ( <Link name="Rocketseat" url="https://rocketseat.com.br" onDetails={() => console.log("Clicou!")} />)}
     
+
+
+    style={styles.links}
+    contentContainerStyle={styles.linksContent}
+    showsVerticalScrollIndicator  ={false}
+
     />
+    <Modal transparent visible={true}>
+<View style={styles.modal}>
+  <View style={styles.modalContent}>
+<View style={styles.modalHeader}>
+<Text style={styles.modalcategory}>Curso</Text>S
+<TouchableOpacity>
+<MaterialIcons name="close" size={24} color={colors.gray[400]} />
+</TouchableOpacity>
+</View>
+<Text style={styles.modalLinkName}>Rocketseat</Text>
+<Text style={styles.modalUrl}>https://rocketseat.com.br</Text>
+<View style = {styles.modalFooter}>
+  <Option name= "Excluir" icon="delete" variant="secondary" />
+   <Option name= "Abrir" icon="language"/>
+</View>
+  </View>
+
+</View>
+
+    </Modal>
     </View>
   )
 }
--- links/src/app/index/styles.ts
+++ links/src/app/index/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Modal, StyleSheet } from "react-native";
 
 import { colors } from "@/styles/colors";
 
@@ -24,5 +24,55 @@ fontSize: 22,
   logo:{
     height:32,
     width: 38,
+  },
+  links: {
+    borderTopWidth: 1,
+  borderColor: colors.gray[600],
+  },
+  linksContent: {
+    gap:20,
+    padding: 24,
+    paddingBottom: 100,
+  },
+  modal:{
+flex:1,
+justifyContent : "flex-end"
+  },
+  modalContent: {
+backgroundColor: colors.gray[900],
+borderTopWidth: 1,
+borderTopColor: colors.gray[800],
+paddingBottom: 42,
+padding: 24,
+  },
+  modalHeader:{
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  modalcategory:{
+    flex: 1,
+    fontSize: 16,
+    fontWeight:"500",
+    color: colors.gray[400],
+  },
+  modalLinkName:{
+    fontSize: 18,
+    fontWeight: "600",
+    color: colors.gray[200],
+  },
+  modalUrl:{
+    fontSize: 14,
+    color: colors.gray[400],
+  },
+  modalFooter:{
+    flexDirection: "row",
+    marginTop: 32,
+    width: "100%",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+    paddingVertical: 14,
   }
-})
+});
